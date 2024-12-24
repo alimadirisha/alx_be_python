5745248ae3d316ae778c27e2023eae7483145e65
comaradeali <alimadiisha30@gmail>
2024-12-24 13:02:01 +0300

    Modified the script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,17 @@
  # Prompt for task description
-while True:
-    Task = input("Enter your task: ")
-    if Task:
-        break
+Task = input("Enter your task: ")
+if not Task:
     print("Task description cannot be empty!")
 
+
 # Prompt for task priority
-while True:
-    Priority = input("Enter your task's priority (high/medium/low): ")
-    if Priority in ["high", "medium", "low"]:
-        break  # Exit the loop if the input is valid
+Priority = input("Enter your task's priority (high/medium/low): ")
+if Priority not in ["high", "medium", "low"]:
     print("Invalid priority! Please enter 'high', 'medium', or 'low'.")
     
  # Prompt for time sensitivity
-while True:
-    Time_bound = input("Is it time-bound? (yes/no): ")
-    if Time_bound in ["yes", "no"]:
-        break  # Exit the loop if the input is valid
+Time_bound = input("Is it time-bound? (yes/no): ")
+if Time_bound not in ["yes", "no"]:
     print("Invalid response! Please enter 'yes' or 'no'.")
 
 # Process the task based on priority using match case
