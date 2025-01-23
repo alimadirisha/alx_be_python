060a6234319fe87946a9d8960a830bf200138c3b
comaradeali <alimadiisha30@gmail>
2025-01-23 13:26:29 +0300

    Modified the script bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,22 +3,15 @@ class BankAccount:
         self.account_balance = account_balance
         
     def deposit(self, amount):
-        if amount > 0:
-            self.account_balance += amount
-            print(f" Deposited: ${self.account_balance:.1f} ")
-        else:
-            print("Deposit amount must be positive")
+        self.account_balance += amount
+        print(f" Deposited: ${self.account_balance:.1f} ")
+       
         
     def withdraw(self, amount):
         if amount > self.account_balance:
-            print("Insufficient funds")
-            return False
-        elif amount <= 0:
-            Print("You can't withdraw zero or negative amount")
-            return False
+            return True
         else:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
                 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")   
